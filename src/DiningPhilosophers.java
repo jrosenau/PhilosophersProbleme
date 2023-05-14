8c4d15ba2c585517a1838b216cb3cad1eb6f1988
jrosenau <jrosenau@htl-steyr.ac.at>
2023-05-15 00:50:00 +0200

    initioaL COMMIT

--- src/DiningPhilosophers.java
+++ src/DiningPhilosophers.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class DiningPhilosophers {
+public class DiningPhilosophers {
+    public static void main(String[] args) {
+        int numberOfPhilosophers = 5;
+        Philosopher[] philosophers = new Philosopher[numberOfPhilosophers];
+        Object[] forks = new Object[numberOfPhilosophers];
+
+        for (int i = 0; i < numberOfPhilosophers; i++) {
+            forks[i] = new Object(); // Each fork is an individual lock
+        }
+
+        for (int i = 0; i < numberOfPhilosophers; i++) {
+            Object leftFork = forks[i];
+            Object rightFork = forks[(i + 1) % numberOfPhilosophers];
+            philosophers[i] = new Philosopher(i, leftFork, rightFork);
+            philosophers[i].start();//test
+        }
+    }
 }
--- src/Philosopher.java
+++ src/Philosopher.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class Philosopher {
+class Philosopher extends Thread {
+    private int id;
+    private Object leftFork;
+    private Object rightFork;
+
+    public Philosopher(int id, Object leftFork, Object rightFork) {
+        this.id = id;
+        this.leftFork = leftFork;
+        this.rightFork = rightFork;
+    }
+
+    private void think() {
+        try {
+            System.out.println("Philosopher " + id + " is thinking");
+            Thread.sleep((int) (Math.random() * 100));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void eat() {
+        synchronized (leftFork) {
+            System.out.println("Philosopher " + id + " picked up left fork");
+            synchronized (rightFork) {
+                System.out.println("Philosopher " + id + " picked up right fork and is eating");
+                try {
+                    Thread.sleep((int) (Math.random() * 100));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Philosopher " + id + " finished eating");
+            }
+            System.out.println("Philosopher " + id + " put down right fork");
+        }
+        System.out.println("Philosopher " + id + " put down left fork");
+    }
+//test
+    public void run() {
+        while (true) {
+            think();
+            eat();
+        }
+    }
 }
